f0156bc22525f0fd8b69cdb30eeb253766aa8509
Huang-Laurent <82091053+Huang-Laurent@users.noreply.github.com>
2022-05-03 22:40:13 +0200

    Add files via upload

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,86 +1,68 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
-#include <QBuffer>
-#include <QFileDialog>
-#include <QImage>
-#include <QInputDialog>
 
-
-/* Constructeur fenetre principale */
+/* Constructeur fenêtre principale */
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow),
     isJoined_(false),
     count_image(0)
 {
-    /* Setup fenetre */
+    /* Setup fenêtre principale */
     ui->setupUi(this);
     this->setWindowTitle("Messagerie Multicast");
-    ui->lineEdit->setText("Anonyme");
+    ui->lineEdit_pseudo->setText("Anonyme");
+
 
     /* Setup socket UDP */
     socket = new QUdpSocket(this);
     socket->setSocketOption(QAbstractSocket::MulticastTtlOption, 1);
-
     if (!socket->bind(QHostAddress(MULTICAST_ADDRESS), MULTICAST_PORT, QUdpSocket::ReuseAddressHint|QUdpSocket::ShareAddress))
         exit(EXIT_FAILURE);
     socket->joinMulticastGroup((QHostAddress(MULTICAST_ADDRESS)));
+    connect(socket, &QUdpSocket::readyRead, this, &MainWindow::processDatagram);
 
-    connect(socket, &QUdpSocket::readyRead, this, &MainWindow::processPendingDatagram);
 
-    /* Creation d'un dossier temporaire pour fichiers */
-    QDir dir_tmp("./tmp");
+    /* Création d'un dossier temporaire pour stockage des images */
+    QDir dir_tmp;
+    dir_tmp.cd("./tmp");
     if (!dir_tmp.exists())
-        dir_tmp.mkdir("./tmp");
+    {
+        if (!dir_tmp.mkdir("./tmp"))
+            qDebug() << "dossier tmp non cree" << endl;
+    }
 
-    /* Pop-up pseudo au demarrage */
+
+    /* Pop-up pseudo du démarrage */
     bool ok;
-    QString text = QInputDialog::getText(this,
-                                         tr("Messagerie Multicast"),
-                                         tr("Choisissez votre pseudo"),
-                                         QLineEdit::Normal,
-                                         QDir::home().dirName(),
-                                         &ok);
+    QString text = QInputDialog::getText(this, tr("Bienvenue"), tr("Choisissez votre pseudo :"), QLineEdit::Normal, QDir::home().dirName(), &ok);
     if (ok and !text.isEmpty())
-        ui->lineEdit->setText(text);
+        ui->lineEdit_pseudo->setText(text);
     else
-        ui->lineEdit->setText("Anonyme");
-    ui->lineEdit->setReadOnly(true);
+        ui->lineEdit_pseudo->setText("Anonyme");
+    ui->lineEdit_pseudo->setDisabled(true);
 
-    /* Prevenir les autres utilisateurs l'arivee */
+
+    /* Message d'accueil */
+    ui->textBrowser->clear();
+    ui->textBrowser->append("Bienvenue " + ui->lineEdit_pseudo->text().toUtf8() + " :) Ici on ne chat pas, on multicast !");
+
+
+    /* Prévenir l'arrivée aux autres utilisateurs */
     on_pushButton_Send_clicked();
 }
 
 
-/* Destructeur fenetre principale */
+/* Destructeur fenêtre principale */
 MainWindow::~MainWindow()
 {
-    /* Destruction dossier tmp */
-//    QString path = "./tmp/";
-//    if (path.isEmpty())
-//          return;
-//    QDir dir(path);
-//    if(!dir.exists())
-//        return;
-//    dir.setFilter(QDir::AllEntries | QDir::NoDotAndDotDot);
-//    QFileInfoList fileList = dir.entryInfoList();
-//    foreach (QFileInfo file, fileList){
-//        if (file.isFile()){
-//            bool isDelete = file.dir().remove(file.fileName());
-//            if(isDelete) {
-//                m_fileCount++;
-//            }
-//            qDebug() << "isDelete= " << isDelete << " m_fileCount = " << m_fileCount << " filename= " << file.fileName();
-//        }else{
-//            deleteDir(file.absoluteFilePath());
-//        }
-//    }
-//    dir.rmpath(dir.absolutePath());
+    /* Destruction du dossier tmp */
+    // à faire
 
     /* Prevenir les autres utilisateurs le depart */
-    QByteArray datagram = ui->lineEdit->text().toUtf8();
-    datagram = datagram + " s'est déconnecté :(";
+    QByteArray datagram = ui->lineEdit_pseudo->text().toUtf8();
+    datagram = datagram +  ": :déconnecté#147258!@.";
     socket->writeDatagram(datagram, QHostAddress(MULTICAST_ADDRESS), MULTICAST_PORT);
 
     /* Destruction UI */
@@ -98,17 +80,28 @@ void MainWindow::setisJoined(const bool& isJoined)
 /* Slot bouton "Envoyer" */
 void MainWindow::on_pushButton_Send_clicked()
 {
+    /* Envoi de message */
     if (isJoined_ == true)
     {
         QByteArray message = ui->textEdit->toPlainText().toUtf8();
-        QByteArray datagram = ui->lineEdit->text().toUtf8();
+        QByteArray datagram = ui->lineEdit_pseudo->text().toUtf8() + " :" + QByteArray::number(anonymousNumber);
         datagram = datagram + " : " + message;
         socket->writeDatagram(datagram, QHostAddress(MULTICAST_ADDRESS), MULTICAST_PORT);
         ui->textEdit->clear();
-    } else
+        // qDebug() << datagram << endl;
+    }
+
+    /* Envoi du tout premier message & calcul du numéro anonyme */
+    else
     {
-        QByteArray datagram = ui->lineEdit->text().toUtf8();
-        datagram = datagram + ":arejoint#147258!@.";
+        if (ui->lineEdit_pseudo->text().toUtf8() == "Anonyme")
+        {
+            qsrand(QTime(0,0,0).secsTo(QTime::currentTime()));
+            anonymousNumber = qrand()%100;
+        } else
+            anonymousNumber = 0;
+        QByteArray datagram = ui->lineEdit_pseudo->text().toUtf8() + ":" + QByteArray::number(anonymousNumber);
+        datagram = datagram + ":arejoint#147258!@.";    // Code du premier message
         socket->writeDatagram(datagram, QHostAddress(MULTICAST_ADDRESS), MULTICAST_PORT);
         ui->textEdit->clear();
         setisJoined(true);
@@ -116,70 +109,93 @@ void MainWindow::on_pushButton_Send_clicked()
 }
 
 
-/* Slot reception message */
-void MainWindow::processPendingDatagram()
+/* Slot réception message */
+void MainWindow::processDatagram()
 {
+    QByteArray long_image_datagram;
     while (socket->hasPendingDatagrams())
     {
         QByteArray datagram;
-        QHostAddress des;
-        quint16 peerPort;
+        QHostAddress host;
+        quint16 port;
         datagram.resize(socket->pendingDatagramSize());
-        socket->readDatagram(datagram.data(), datagram.size(), &des, &peerPort);
-        if (datagram.size() < 2000)
+        socket->readDatagram(datagram.data(), datagram.size(), &host, &port);
+
+        /* Réception grande image */
+        if (QString(datagram.data()).split(":")[0] == "#147258!@.img")
         {
-            QStringList msg_split = QString(datagram.data()).split(":");
-            if (msg_split[0] == "Anonyme " and msg_split[1] != "/image#147258!@.")
-            {
-                ui->textBrowser->append("Anonyme " + QString::number(peerPort) + " :" + msg_split[1]);
-            } else if (msg_split[0] == "Anonyme" and msg_split[1] == "/image#147258!@.")
-            {
-                ui->textBrowser->append("Anonyme " + QString::number(peerPort) + " a envoyé une image.");
-                ui->label_envoyeurImage->setText("Image envoyée par Anonyme " + QString::number(peerPort));
-            } else if (msg_split[0] != "Anonyme" and msg_split[1] == "/image#147258!@.")
-            {
-                ui->textBrowser->append(msg_split[0] + " a envoyé une image.");
-                ui->label_envoyeurImage->setText("Image envoyée par " + msg_split[0]);
-            } else if (msg_split[0] != "Anonyme" and msg_split[1] == "arejoint#147258!@.")
-            {
-                ui->textBrowser->append(msg_split[0] + " a rejoint la discussion !");
-            } else if (msg_split[0] == "Anonyme" and msg_split[1] == "arejoint#147258!@.")
-            {
-                ui->textBrowser->append("Anonyme " + QString::number(peerPort) + " a rejoint la discussion !");
-            } else
-            {
-                ui->textBrowser->append(datagram.data());
-            }
-        } else
+            // Rellocage des paquets
+            long_image_datagram.append(QString(datagram.data()).split(":")[1]);
+            qDebug() << "test" << endl;
+            qDebug() << long_image_datagram.size() << endl;
+        } else if (QString(datagram.data()).split(":")[0] == "#147258!@.endlongimg")
         {
-            QImage image_temp = get_imagedata_from_byte(datagram.data());
+            // Reception du dernier paquet et affichage de l'image
+            qDebug() << "test2" << endl;
+            long_image_datagram.append(QString(datagram.data()).split(":")[1]);
+            QImage image_temp = image_to_byte(QString(long_image_datagram.data()));
             image_temp.save(QString("./tmp/image%1.png").arg(count_image), "PNG");
             QImage* imgScaled = new QImage;
             *imgScaled = image_temp.scaled(400, 400, Qt::KeepAspectRatio);
             ui->label_image->setPixmap(QPixmap::fromImage(*imgScaled));
-            // ui->label_image->setPixmap(QPixmap::fromImage(image_temp).scaled(ui->label_image->size()));
             count_image++;
+            break;
+        }
+
+        /* Réception et affichage petite image */
+        if (QString(datagram.data()).split(":")[0] == "#147258!@.endimg")
+        {
+            QImage image_temp = image_to_byte(QString(datagram.data()).split(":")[1]);
+            image_temp.save(QString("./tmp/image%1.png").arg(count_image), "PNG");
+            QImage* imgScaled = new QImage;
+            *imgScaled = image_temp.scaled(400, 400, Qt::KeepAspectRatio);
+            ui->label_image->setPixmap(QPixmap::fromImage(*imgScaled));
+            count_image++;
+            break;
+        }
+
+        /* Réception et affichage de message textuel */
+        else if (QString(datagram.data()).split(":")[0] != "#147258!@.endimg" and QString(datagram.data()).split(":")[0] != "#147258!@.endlongimg" and QString(datagram.data()).split(":")[0] != "#147258!@.img")
+        {
+            QStringList msg_split = QString(datagram.data()).split(":");
+            QTime time = QTime::currentTime();
+            if (msg_split[0] == "Anonyme " and msg_split[2] != "/image#147258!@.")
+            {
+                ui->textBrowser->append(time.toString() + " | " + "Anonyme " + QString::number(port+msg_split[1].toInt()) + " :" + msg_split[2]);
+            } else if (msg_split[0] == "Anonyme" and msg_split[2] == "/image#147258!@.")
+            {
+                ui->textBrowser->append(time.toString() + " | " + "Anonyme " + QString::number(port+msg_split[1].toInt()) + " a envoyé une image.");
+                ui->label_envoyeurImage->setText("Image envoyée par Anonyme " + QString::number(port+msg_split[1].toInt()));
+            } else if (msg_split[0] != "Anonyme" and msg_split[2] == "/image#147258!@.")
+            {
+                if (msg_split[0] == ui->lineEdit_pseudo->text().toUtf8())
+                {
+                    msg_split[0] = "Vous";
+                    ui->textBrowser->append(time.toString() + " | " + "Vous avez envoyé une image.");
+                    ui->label_envoyeurImage->setText("Image envoyée par vous");
+                } else
+                {
+                    ui->textBrowser->append(time.toString() + " | " + msg_split[0] + " a envoyé une image.");
+                    ui->label_envoyeurImage->setText("Image envoyée par " + msg_split[0]);
+                }
+            } else if (msg_split[0] != "Anonyme" and msg_split[2] == "arejoint#147258!@.")
+            {
+                if (!(msg_split[0] == ui->lineEdit_pseudo->text().toUtf8()))
+                    ui->textBrowser->append(time.toString() + " | " + msg_split[0] + " a rejoint la discussion !");
+            } else if (msg_split[0] == "Anonyme" and msg_split[2] == "arejoint#147258!@.")
+            {
+                ui->textBrowser->append(time.toString() + " | " + "Anonyme " + QString::number(port+msg_split[1].toInt()) + " a rejoint la discussion !");
+            } else if (msg_split[2] == "déconnecté#147258!@.")
+            {
+                ui->textBrowser->append(time.toString() + " | " + msg_split[0] + " s'est déconnecté :(");
+            } else
+            {
+                if (msg_split[0] == ui->lineEdit_pseudo->text().toUtf8() + " ")
+                    msg_split[0] = "Vous ";
+                ui->textBrowser->append(time.toString() + " | " + msg_split[0]  + ":" + msg_split[2]);
+            }
         }
     }
-}
-
-
-QByteArray MainWindow::get_imagedata_from_imagefile(const QImage &image)
-{
-    QByteArray imageData;
-    QBuffer buffer(&imageData);
-    image.save(&buffer, "JPG");
-    imageData = imageData.toBase64();
-    return imageData;
-}
-
-
-QImage MainWindow::get_imagedata_from_byte(const QString &data)
-{
-    QByteArray imageData = QByteArray::fromBase64(data.toLatin1());
-    QImage image;
-    image.loadFromData(imageData);
-    return image;
 }
 
 
@@ -190,24 +206,72 @@ void MainWindow::on_pushButton_Send_Image_clicked()
     if (file_name == "")
         return;
 
+    QImage image;
     image.load(file_name);
-    ui->label_image->setPixmap(QPixmap::fromImage(image).scaled(ui->label_image->size()));
+    QString image_file_data = byte_from_imagefile(image);
 
-    image_file_data = get_imagedata_from_imagefile(image);
-    socket->writeDatagram(image_file_data.toLatin1(), image_file_data.size(), QHostAddress(MULTICAST_ADDRESS), MULTICAST_PORT);
+    /* Envoi d'image de taille inférieure à 65000o */
+    if (image_file_data.size() < 65000)
+    {
+        socket->writeDatagram("#147258!@.endimg:" + image_file_data.toLatin1(), QHostAddress(MULTICAST_ADDRESS), MULTICAST_PORT);
+    }
 
-    QByteArray datagram = ui->lineEdit->text().toUtf8();
-    datagram += ":/image#147258!@.";
+    /* Envoi d'image de taille importante en plusieurs paquets */
+    else
+    {
+        int i = 0;
+        QString package;
+        QString long_image_file_data = byte_from_imagefile(image);
+        long_image_file_data = image_file_data.mid(1 + i * 64500);
+        while (long_image_file_data.size() > 64500)
+        {
+            // Decoupage en paquets
+            package = "#147258!@.img:" + image_file_data.mid(1 + i * 64500, 64500);
+            socket->writeDatagram(package.toLatin1(), QHostAddress(MULTICAST_ADDRESS), MULTICAST_PORT);
+            i++;
+            long_image_file_data = image_file_data.mid(1 + i * 64500);
+            qDebug() << long_image_file_data.size() << endl;
+        }
+        package = "#147258!@.endlongimg:" + image_file_data.mid(1 + i * 64500);  // Dernier paquet
+        socket->writeDatagram(package.toLatin1(), QHostAddress(MULTICAST_ADDRESS), MULTICAST_PORT);
+    }
+    qDebug() << "go" << endl;
+    QByteArray datagram = ui->lineEdit_pseudo->text().toUtf8() + ":" + QByteArray::number(anonymousNumber);
+    datagram += ":/image#147258!@.";  // Code pour envoi d'image
     socket->writeDatagram(datagram, QHostAddress(MULTICAST_ADDRESS), MULTICAST_PORT);
 }
 
 
 /* Slot bouton "Enregistrer l'image" */
-void MainWindow::on_pushButton_Send_Image_2_clicked()
+void MainWindow::on_pushButton_Save_Image_clicked()
 {
     QString file_path = QFileDialog::getExistingDirectory(this, "Enregistrer sous...", "./");
     QImage image_temp = QImage(QString("./tmp/image%1.png").arg(count_image-1));
     if (!image_temp.save(file_path + QString("/image%1.png").arg(count_image-1), "PNG"))
-        // qDebug() << file_path + QString("/image%1.png").arg(count_image-1) << endl;
+    {
+        ui->textBrowser->append(QString("image%1.png n'a pas pu être enregistré.").arg(count_image-1));
         return;
+    }
+    ui->textBrowser->append("L'image a été enregistrée sous " + file_path + QString("/image%1.png").arg(count_image-1));
+}
+
+
+/* Convertir image en QByteArray */
+QByteArray MainWindow::byte_from_imagefile(const QImage &image)
+{
+    QByteArray imageData;
+    QBuffer buffer(&imageData);
+    image.save(&buffer, "JPG");
+    imageData = imageData.toBase64();
+    return imageData;
+}
+
+
+/* Convertir imgage en QImage */
+QImage MainWindow::image_to_byte(const QString &data)
+{
+    QByteArray imageData = QByteArray::fromBase64(data.toLatin1());
+    QImage image;
+    image.loadFromData(imageData);
+    return image;
 }
--- mainwindow.h
+++ mainwindow.h
@@ -5,48 +5,86 @@
 #include <QUdpSocket>
 #include <QImage>
 #include <QBuffer>
+#include <QtGlobal>
+#include <QTime>
+#include <QFileDialog>
+#include <QInputDialog>
 
 
+/* Adresse D et port qu'on va utiliser */
 #define MULTICAST_ADDRESS "239.255.255.252"
 #define MULTICAST_PORT    12345
 
 
-namespace Ui {
+namespace Ui{
 class MainWindow;
 }
+
 
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
 public:
+    /* Constructeur fenêtre principale */
     explicit MainWindow(QWidget *parent = 0);
+
+    /* Destructeur fenêtre principale */
     ~MainWindow();
 
+
 public:
+    /* Constructeur bool isJoined_ */
     void setisJoined(const bool& isJoined);
 
+
 private slots:
-    void processPendingDatagram();
+    /* Slot réception message
+     * Lecture, traitement et affichage des messages reçus.
+     * On distingue plusieurs cas de figure en fonction du message codé reçu.
+     * Le codage a pour but de différencier les messages spéciaux (messages d'arrivée/de départ, images...) avec les messages textuels normaux.
+     * Les messages sont composés de 3 parties séparées par des ":" au format "pseudo:numero anonyme:message utile". */
+    void processDatagram();
+
+    /* Slot bouton "Envoyer" */
     void on_pushButton_Send_clicked();
+
+    /* Slot bouton "Envoyer une image" */
     void on_pushButton_Send_Image_clicked();
-    void on_pushButton_Send_Image_2_clicked();
+
+    /* Slot bouton "Enregistrer l'image" */
+    void on_pushButton_Save_Image_clicked();
+
 
 private:
     Ui::MainWindow *ui;
+
+    /* Socket UDP */
     QUdpSocket* socket;
 
+
 private:
+    /* Si isJoined_ = false, l'utilisateur annonce à tout le monde son arrivée. Ce bool ne sera donc utilisé qu'une fois */
     bool isJoined_;
+
+    /* Le numéro anonyme sert à différencier les anonymes.
+     * Ce nombre est généré grâce à un nombre pseudo-aléatoire.
+     * Il sera additionné au numéro de port avant d'être affiché sur la fenêtre de chat.
+     * Il vaut 0 si l'utilisateur n'est pas anonyme.
+     * Un utilisateur est anonyme s'il clique sur "cancel" lors du choix du pseudo */
+    int anonymousNumber;
+
+    /* count_image sert à numéroter les images.
+     * Il est utilisé lors du nommage des images. */
     int count_image;
-    QString image_file_data;
-    QImage image;
-    QString picture_data;
+
 
 private:
-    QByteArray get_imagedata_from_imagefile(const QImage &image);
-    QImage get_imagedata_from_byte(const QString &data);
+    /* Convertir image en QByteArray */
+    QByteArray byte_from_imagefile(const QImage &image);
 
+    /* Convertir imgage en QImage */
+    QImage image_to_byte(const QString &data);
 };
 
 #endif // MAINWINDOW_H
